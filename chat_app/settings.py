9730e3b361ee3495d0850c16c57235df726f1c18
seita-uchida <seita_uchida@waotech.jp>
2025-08-29 01:23:30 +0900

    2-会員登録機能の作成

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ファイルの末尾に以下を追加
+AUTH_USER_MODEL = "main.user"  # User のモデルがどれかを指定する
