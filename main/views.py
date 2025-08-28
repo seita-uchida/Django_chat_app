07f1956f0a72ce5305d7970da0f5f4923a4e5d62
seita-uchida <seita_uchida@waotech.jp>
2025-08-29 01:14:47 +0900

    1-チャットアプリを作ろう

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "main/index.html")
+
+
+def signup(request):
+    return render(request, "main/signup.html")
+
+
+def login(request):
+    return render(request, "main/login.html")
